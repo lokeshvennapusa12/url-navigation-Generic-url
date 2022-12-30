fef3dccc0072524cdfb9459330bd0ba4fd9c2a89
lokeshvennapusa12 <lokeshvennapusa12@gmail.com>
2022-12-30 13:40:20 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+def loki1(request):
+    return render(request,'loki1.html')
+    
---
+++ app2/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def loki2(request):
+    return render(request,'loki2.html')
